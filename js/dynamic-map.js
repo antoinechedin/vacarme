71499ec994b3ed3d726531c324d193d70c41ee3e
antoinechedin <antoine.chedin@gmail.com>
2022-01-05 14:08:15 +0100

    load local map

--- js/dynamic-map.js
+++ js/dynamic-map.js
@@ -1,1 +1,13 @@
-var map = L.map('dynamic-map').setView([51.505, -0.09], 13);
+var map = L.map('dynamic-map', {
+    minZoom: 0,
+    maxZoom: 2
+});
+
+L.tileLayer('http://localhost/wp-content/uploads/2022/01/{z}{y}{x}.jpg', {
+    minZoom: 0,
+    maxZoom: 2,
+    noWrap: true,
+    bounds: L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180))
+}).addTo(map);
+
+map.setView([0, 0], 0);
